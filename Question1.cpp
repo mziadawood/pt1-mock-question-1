809056a38ab8d4b52031c46c9569919ace72b350
mziadawood <mziadawood@gmail.com>
2015-04-13 22:19:09 +0200

    1.3

--- Question1.cpp
+++ Question1.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
 	// 1.3 switch to the "master" branch, uncomment the following four lines and make a commit
 	 string name;
 	 cout << "Please enter your name: ";
- cin >> name;
+     cin >> name;
 	 cout << "Hello, " << name << endl;
 
 	// 1.4 merge branch "hello" into "master" keeping ALL of the code added so far
